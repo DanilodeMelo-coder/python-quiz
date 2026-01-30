3c0441dabe0235be5c62d69b9249e5fbee710a19
DanilodeMelo-coder <danilo.demelo.0207@gmail.com>
2026-01-30 02:20:40 -0300

    feat: quiz v2 com funções, porcentagem e desempenho

--- Exercicio_foco.py
+++ Exercicio_foco.py
@@ -1,3 +1,5 @@
+import os
+from time import sleep
 
 quiz = [
     {
@@ -14,24 +16,80 @@ quiz = [
 
 ]
 
-acertos = 0
+def rodar_quiz():
+    acertos = 0
+    erros = 0
 
-for pergunta in quiz:
-    print(pergunta["Pergunta"])
+    for pergunta in quiz:
+        print(pergunta["Pergunta"])
 
-    for i, opcao in enumerate(pergunta["Opções"]):
-        print(f"{i}) {opcao}")
+        for i, opcao in enumerate(pergunta["Opções"]):
+            print(f"{i}) {opcao}")
 
-    resposta_user = int(input("Escolha uma das alternativas: "))
+        resposta_user = input("Escolha uma das alternativas: ")
 
-    if resposta_user < 0 or resposta_user >= len(pergunta["Opções"]):
-        print("Resposta invalida")
+        if resposta_user.isdigit() != True:
+            print("Digite apenas numeros!!")
+            continue
+        else:
+            resposta_user = int(resposta_user)
+
+        if resposta_user < 0 or resposta_user >= len(pergunta["Opções"]):
+            print("Resposta invalida")
+            continue
+
+        if pergunta["Opções"][resposta_user] == pergunta["Resposta"]:
+            print("Você acertou, parabéns!!!!")
+            acertos += 1
+        else:
+            print("Você errou, que pena")
+            erros += 1
+
+    porcentagem_final = (acertos/len(quiz)) * 100
+
+    return acertos, erros, porcentagem_final
+
+def menu_final(acertos, erros, porcentagem_final):
+    print(f"Você acertou {acertos} de {len(quiz)} perguntas")
+    print(f"Você errou {erros} de {len(quiz)} perguntas")
+    print(f"Sua porcentagem final foi de {porcentagem_final}%")
+
+def desempenho(porcentagem_final):
+    if porcentagem_final >= 80:
+        print(f"Parabéns, obteve um ótimo desempenho")
+    elif porcentagem_final >= 50:
+        print(f"Pode melhorar, obteve desempenho médio")
+    else:
+        print(f"Estude mais, você teve um desempenho ruim")
+
+def sair_ou_continuar():
+    sair_continuar = input("Deseja continuar? [S]im/[N]ão ")
+    return sair_continuar.startswith("s") 
+
+while True:
+    acertos, erros, porcentagem_final = rodar_quiz()
+    menu_final(acertos, erros, porcentagem_final)
+
+    desempenho(porcentagem_final)
+
+    if sair_ou_continuar() == True:
+        print()
+        acertos = 0
+        erros = 0
+
+        print("REINCIANDO PROGRAMA...")
+
+        sleep(1.0)
+
+        os.system("cls")
+
         continue
 
-    if pergunta["Opções"][resposta_user] == pergunta["Resposta"]:
-        print("Você acertou, parabéns!!!!")
-        acertos += 1
     else:
-        print("Você errou, que pena")
+        print()
 
-print(f"Você acertou {acertos} de {len(quiz)} perguntas")
+        print("ENCERRANDO PROGRAMA...")
+
+        break
+
+
